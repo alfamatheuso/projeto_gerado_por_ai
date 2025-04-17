c94dbf2fb015f1dd1ec4a87b12572d7d9f5c33f7
Matheus Oliveira <matheuso@alfaconsultorias.com.br>
2025-04-17 13:58:15 -0300

    novo teste

--- lib/features/login/page/login_page.dart
+++ lib/features/login/page/login_page.dart
@@ -1,40 +1,39 @@
+import 'package:flutter/material.dart';
+import '../controller/login_controller.dart';
+import 'components/login_button.dart';
+import 'components/register_text.dart';
+
 class LoginPage extends StatelessWidget {
-  final LoginController controller = LoginController();
-
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-
-  LoginPage({super.key});
+  final LoginController _controller = LoginController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text("Login")),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(labelText: 'Senha'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                controller.login(
-                  context,
-                  emailController.text,
-                  passwordController.text,
-                );
-              },
-              child: Text('Login'),
-            ),
-          ],
+        child: Form(
+          key: _controller.formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _controller.emailController,
+                decoration: InputDecoration(labelText: "Email"),
+                validator: (value) => value!.isEmpty ? "Digite o email" : null,
+              ),
+              TextFormField(
+                controller: _controller.passwordController,
+                decoration: InputDecoration(labelText: "Senha"),
+                obscureText: true,
+                validator: (value) => value!.isEmpty ? "Digite a senha" : null,
+              ),
+              SizedBox(height: 20),
+              LoginButton(onPressed: _controller.login),
+              SizedBox(height: 10),
+              RegisterText(onPressed: () => _controller.navigateToRegister(context)),
+            ],
+          ),
         ),
       ),
     );
