d41c3bbb14a7f738a066a6546dc1e462ea967bba
Matheus Oliveira <matheuso@alfaconsultorias.com.br>
2025-04-17 14:09:37 -0300

    novo teste

--- lib/features/register/model/register_model.dart
+++ lib/features/register/model/register_model.dart
@@ -1,19 +1,11 @@
 class RegisterModel {
-  final String name;
   final String email;
   final String password;
+  final String confirmPassword;
 
   RegisterModel({
-    required this.name,
     required this.email,
     required this.password,
+    required this.confirmPassword,
   });
-
-  Map<String, dynamic> toJson() {
-    return {
-      'name': name,
-      'email': email,
-      'password': password,
-    };
-  }
 }
