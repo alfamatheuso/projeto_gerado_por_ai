7f949cd396eb34fc6e9107f48a01d27d1967e15a
Matheus Oliveira <matheuso@alfaconsultorias.com.br>
2025-04-17 13:43:58 -0300

    novo teste

--- lib/features/login/model/login_model.dart
+++ lib/features/login/model/login_model.dart
@@ -1,6 +1,13 @@
 class LoginModel {
-  String username;
-  String password;
+  final String email;
+  final String password;
 
-  LoginModel({this.username = '', this.password = ''});
+  LoginModel({required this.email, required this.password});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'password': password,
+    };
+  }
 }
