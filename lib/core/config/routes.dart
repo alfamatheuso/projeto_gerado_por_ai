8fc11a06e9366fa8fcb426a0c3de8f5e165cf616
Matheus Oliveira <matheuso@alfaconsultorias.com.br>
2025-04-17 11:57:31 -0300

    novo teste

---
+++ lib/core/config/routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../../features/home/page/home_page.dart';
+
+class AppRoutes {
+  static final routes = {
+    '/': (context) => const HomePage(),
+  };
+}
