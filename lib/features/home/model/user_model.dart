cf43898afe29833e83e6c9ee212cfd1f5e28e384
Matheus Oliveira <matheuso@alfaconsultorias.com.br>
2025-04-17 11:31:15 -0300

    novo teste

---
+++ lib/features/home/model/user_model.dart
@@ -0,0 +1,13 @@
+class UserModel {
+  final String id;
+  final String username;
+  final String profilePicture;
+  final String bio;
+
+  UserModel({
+    required this.id,
+    required this.username,
+    required this.profilePicture,
+    required this.bio,
+  });
+}
