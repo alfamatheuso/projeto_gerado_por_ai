5794c053f0760fd6f1ecbb94d21342cf30ac33e3
Matheus Oliveira <matheuso@alfaconsultorias.com.br>
2025-04-17 14:15:21 -0300

    Na logica da tela de registre-se valide todos campos obrigatórios, caso seja aprovado navegue para home etc.

--- lib/features/register/model/register_model.dart
+++ lib/features/register/model/register_model.dart
@@ -1,9 +1,11 @@
 class RegisterModel {
-  final String email;
-  final String password;
-  final String confirmPassword;
+  String name;
+  String email;
+  String password;
+  String confirmPassword;
 
   RegisterModel({
+    required this.name,
     required this.email,
     required this.password,
     required this.confirmPassword,
