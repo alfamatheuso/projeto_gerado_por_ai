b83e8face45ee5186d40ec42a812ea43234aff4f
Matheus Oliveira <matheuso@alfaconsultorias.com.br>
2025-04-17 13:58:14 -0300

    novo teste

--- lib/features/login/controller/login_controller.dart
+++ lib/features/login/controller/login_controller.dart
@@ -1,16 +1,18 @@
+import 'package:flutter/material.dart';
+
 class LoginController {
-  final LoginRepository repository = LoginRepository();
+  final formKey = GlobalKey<FormState>();
 
-  Future<void> login(BuildContext context, String email, String password) async {
-    final loginModel = LoginModel(email: email, password: password);
-    final success = await repository.login(loginModel);
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
-    if (success) {
-      Navigator.pushReplacementNamed(context, '/home');
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Falha no login. Verifique suas credenciais.')),
-      );
+  void login() {
+    if (formKey.currentState!.validate()) {
+      // Implementar lógica do login
     }
+  }
+
+  void navigateToRegister(BuildContext context) {
+    Navigator.pushNamed(context, '/register');
   }
 }
