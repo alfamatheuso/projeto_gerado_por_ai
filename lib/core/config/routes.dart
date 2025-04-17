824149cd55a75042d9ff2ebdba6daa8b8e5d436d
Matheus Oliveira <matheuso@alfaconsultorias.com.br>
2025-04-17 14:15:23 -0300

    Na logica da tela de registre-se valide todos campos obrigatórios, caso seja aprovado navegue para home etc.

--- lib/core/config/routes.dart
+++ lib/core/config/routes.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import '../../features/login/page/login_page.dart';
-import '../../features/home/page/home_page.dart';
 
-class AppRoutes {
-  static const String login = '/login';
-  static const String home = '/home';
-
-  static Map<String, WidgetBuilder> routes = {
-    login: (context) => LoginPage(),
-    home: (context) => HomePage(),
-  };
-}
+final Map<String, WidgetBuilder> routes = {
+  '/register': (context) => RegisterPage(),
+  '/home': (context) => HomePage(),
+};
